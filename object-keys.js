42300540496218b21e589777879c351df95a9bd3
Joshua C <contact.cjoshua@gmail.com>
2022-04-27 14:01:46 -0700

    preittier

--- object-keys.js
+++ object-keys.js
@@ -1,9 +1,9 @@
 const car = {
-    make: 'Honda',
-    model: 'Accord',
-    year: 2020
-}
+  make: "Honda",
+  model: "Accord",
+  year: 2020,
+};
 
-const keys = Object.keys(car)
+const keys = Object.keys(car);
 
-console.log(keys)
+console.log(keys);
